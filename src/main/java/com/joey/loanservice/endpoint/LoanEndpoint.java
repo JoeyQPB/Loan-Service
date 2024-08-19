c03e15e86564cb7d92b8436e18b92b28258c346d
joey <joey_qpb@hotmail.com>
2024-08-19 16:52:43 -0300

    feat: loanrequest endpoint

--- src/main/java/com/joey/loanservice/endpoint/LoanEndpoint.java
+++ src/main/java/com/joey/loanservice/endpoint/LoanEndpoint.java
@@ -1,2 +1,28 @@
-package com.joey.loanservice.endpoint;public class LoanEndpoint {
+package com.joey.loanservice.endpoint;
+
+import com.joey.loanservice.dtos.CustomLoanResponse;
+import com.joey.loanservice.service.LoanService;
+import io.spring.guides.loanservice.loan.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Endpoint
+public class LoanEndpoint {
+
+    private static final String NAMESPACE_URI = "http://spring.io/guides/loanservice/loan";
+
+    @Autowired
+    private LoanService loanService;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "LoanRequest")
+    @ResponsePayload
+    public LoanResponse requestForLoan (@RequestPayload LoanRequest request) throws Exception {
+        return this.loanService.requestLoan(request);
+    }
 }
