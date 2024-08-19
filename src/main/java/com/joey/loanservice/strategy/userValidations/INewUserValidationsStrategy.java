86971e5699f2b48b36774a982227ebc736fd357b
joey <joey_qpb@hotmail.com>
2024-08-19 16:58:04 -0300

    feat> strategy

--- src/main/java/com/joey/loanservice/strategy/userValidations/INewUserValidationsStrategy.java
+++ src/main/java/com/joey/loanservice/strategy/userValidations/INewUserValidationsStrategy.java
@@ -1,6 +1,5 @@
 package com.joey.loanservice.strategy.userValidations;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import io.spring.guides.loanservice.user.UserInfo;
 
 public interface INewUserValidationsStrategy {
