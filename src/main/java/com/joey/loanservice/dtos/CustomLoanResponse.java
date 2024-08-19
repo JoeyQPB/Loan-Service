72c96d33aed08ba10fda2d9df4bd39b3b609b13c
joey <joey_qpb@hotmail.com>
2024-08-19 16:53:15 -0300

    feat> custom loan response pattern

--- src/main/java/com/joey/loanservice/dtos/CustomLoanResponse.java
+++ src/main/java/com/joey/loanservice/dtos/CustomLoanResponse.java
@@ -1,2 +1,13 @@
-package com.joey.loanservice.dtos;public class CustomLoanResponse {
+package com.joey.loanservice.dtos;
+
+import io.spring.guides.loanservice.loan.LoanResponse;
+import io.spring.guides.loanservice.loan.LoanType;
+
+import java.util.List;
+
+public class CustomLoanResponse extends LoanResponse {
+
+    public void setLoanData(List<LoanType> loanData) {
+        this.loanData = loanData;
+    }
 }
